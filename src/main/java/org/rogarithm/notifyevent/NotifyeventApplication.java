b41aa20cf7b4a74ab924bc8b4834e7d9cdf55cdb
sehoon.gim <sehoongim@gmail.com>
2024-11-25 12:09:32 +0900

    initialize repo

---
+++ src/main/java/org/rogarithm/notifyevent/NotifyeventApplication.java
@@ -0,0 +1,13 @@
+package org.rogarithm.notifyevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rogarithm/notifyevent/NotifyeventApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rogarithm.notifyevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
