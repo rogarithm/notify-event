15147a0bbcf396fdf11464bc2b7821d116b00ccd
sehoon.gim <sehoongim@gmail.com>
2024-11-28 15:58:35 +0900

    테스트 정리한다
     - http method별로 테스트를 묶는다

--- src/test/java/org/rogarithm/notifyevent/web/EventControllerTest.java
+++ src/test/java/org/rogarithm/notifyevent/web/EventControllerTest.java
@@ -1,6 +1,7 @@
 package org.rogarithm.notifyevent.web;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,73 +25,82 @@ class EventControllerTest {
     @Mock
     EventService eventService;
 
-    @DisplayName("하루짜리 종일 이벤트를 추가할 수 있다")
-    @Test
-    public void test_add_1day_event() {
-        LocalDate aDay = LocalDate.of(2024, 11, 25);
-        EventAddRequest request = new EventAddRequest(
-                EventType.HAS_NO_TIME,
-                aDay, aDay,
-                null, null,
-                "1 day event"
-        );
+    @Nested
+    @DisplayName("리소스 추가 핸들러")
+    class HandlePost {
+        @DisplayName("하루짜리 종일 이벤트를 추가할 수 있다")
+        @Test
+        public void test_add_1day_event() {
+            LocalDate aDay = LocalDate.of(2024, 11, 25);
+            EventAddRequest request = new EventAddRequest(
+                    EventType.HAS_NO_TIME,
+                    aDay, aDay,
+                    null, null,
+                    "1 day event"
+            );
 
-        eventController.add(request);
+            eventController.add(request);
 
-        Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+            Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+        }
+
+        @DisplayName("하루 중 일부에 시작하고 끝나는 이벤트를 추가할 수 있다")
+        @Test
+        public void test_add_1day_partial_event() {
+            LocalDate aDay = LocalDate.of(2024, 11, 25);
+            LocalTime startTime = LocalTime.of(20, 0);
+            LocalTime endTime = LocalTime.of(21, 0);
+            EventAddRequest request = new EventAddRequest(
+                    EventType.HAS_TIME,
+                    aDay, aDay,
+                    startTime, endTime,
+                    "1 day partial event"
+            );
+
+            eventController.add(request);
+
+            Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+        }
+
+        @DisplayName("여러 날 짜리 종일 이벤트를 추가할 수 있다")
+        @Test
+        public void test_add_x_day_event() {
+            LocalDate startDay = LocalDate.of(2024, 11, 25);
+            LocalDate endDay = LocalDate.of(2024, 11, 30);
+            EventAddRequest request = new EventAddRequest(
+                    EventType.HAS_NO_TIME,
+                    startDay, endDay,
+                    null, null,
+                    "x day event"
+            );
+
+            eventController.add(request);
+
+            Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+        }
+
+        @DisplayName("시작 시간과 종료 시간이 있고 여러 날이 포함되는 이벤트를 추가할 수 있다")
+        @Test
+        public void test_add_x_day_partial_event() {
+            LocalDate startDay = LocalDate.of(2024, 11, 25);
+            LocalDate endDay = LocalDate.of(2024, 11, 30);
+            LocalTime startTime = LocalTime.of(20, 0);
+            LocalTime endTime = LocalTime.of(21, 0);
+            EventAddRequest request = new EventAddRequest(
+                    EventType.HAS_TIME,
+                    startDay, endDay,
+                    startTime, endTime,
+                    "1 day partial event"
+            );
+
+            eventController.add(request);
+
+            Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+        }
     }
 
-    @DisplayName("하루 중 일부에 시작하고 끝나는 이벤트를 추가할 수 있다")
-    @Test
-    public void test_add_1day_partial_event() {
-        LocalDate aDay = LocalDate.of(2024, 11, 25);
-        LocalTime startTime = LocalTime.of(20, 0);
-        LocalTime endTime = LocalTime.of(21, 0);
-        EventAddRequest request = new EventAddRequest(
-                EventType.HAS_TIME,
-                aDay, aDay,
-                startTime, endTime,
-                "1 day partial event"
-        );
-
-        eventController.add(request);
-
-        Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
-    }
-
-    @DisplayName("여러 날 짜리 종일 이벤트를 추가할 수 있다")
-    @Test
-    public void test_add_x_day_event() {
-        LocalDate startDay = LocalDate.of(2024, 11, 25);
-        LocalDate endDay = LocalDate.of(2024, 11, 30);
-        EventAddRequest request = new EventAddRequest(
-                EventType.HAS_NO_TIME,
-                startDay, endDay,
-                null, null,
-                "x day event"
-        );
-
-        eventController.add(request);
-
-        Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
-    }
-
-    @DisplayName("시작 시간과 종료 시간이 있고 여러 날이 포함되는 이벤트를 추가할 수 있다")
-    @Test
-    public void test_add_x_day_partial_event() {
-        LocalDate startDay = LocalDate.of(2024, 11, 25);
-        LocalDate endDay = LocalDate.of(2024, 11, 30);
-        LocalTime startTime = LocalTime.of(20, 0);
-        LocalTime endTime = LocalTime.of(21, 0);
-        EventAddRequest request = new EventAddRequest(
-                EventType.HAS_TIME,
-                startDay, endDay,
-                startTime, endTime,
-                "1 day partial event"
-        );
-
-        eventController.add(request);
-
-        Mockito.verify(eventService, Mockito.times(1)).add(EventAddDto.from(request));
+    @Nested
+    @DisplayName("리소스 읽어오기 핸들러")
+    class HandleGet {
     }
 }
